ec77366e722d3f6ef8780d9d34c70fc612a9effc
Shamira Jones <shamirajones@shamiras-mbp.home>
2019-10-21 14:59:16 -0400

    Added update and delete

--- controllers/Category.js
+++ controllers/Category.js
@@ -3,34 +3,44 @@ const router = express.Router();
 const CategoryModel = require("../models/Category");
 
 router.get("/", (req, res) => {
-	CategoryModel.find({}).then(categories => {
-		res.json(categories);
-	});
+    CategoryModel.find({}).then(categories => {
+        res.json(categories);
+    });
 });
 
-router.get('/:title', (req,res) => {
-    CategoryModel.findOne({title: req.params.title}).populate('movies').exec((err, pop) => {
+router.get('/:title', (req, res) => {
+    CategoryModel.findOne({ title: req.params.title }).populate('movies').exec((err, pop) => {
         res.json(pop);
     })
 })
 
-router.put('/:title', (req,res) => {
-    CategoryModel.findOneAndUpdate({title: req.params.title}, req.body).then(() => {
+router.put('/:title', (req, res) => {
+    CategoryModel.findOneAndUpdate({ title: req.params.title }, req.body).then(() => {
         res.redirect('/');
     })
 })
 
-router.post('/', (req,res) => {
+router.post('/', (req, res) => {
     CategoryModel.create(req.body).then(newCategory => {
         res.redirect(`/category/${newCategory.title}`);
     })
 })
 
-router.delete('/:title', (req,res) => {
-    CategoryModel.deleteOne({_id: req.params.id}).then(() => {
-        res.redirect('/');
-    })
-})
 
+
+
+router.delete("/:title", (req, res) => {
+    CategoryModel.findOneAndDelete({ title: req.params.title }).then(category => {
+        res.redirect("/");
+    });
+});
+
+
+router.put('/:title', (req, res) => {
+    CategoryModel.findOneAndUpdate({ title: req.params.title }, req.body, { new: true })
+        .then(category => {
+            res.redirect("/");
+        })
+});
 
 module.exports = router;
